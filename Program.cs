d16f282e472b2fa548b1c54a8763f278058fa5a2
Zsófia Limbek <zsofia.limbek@cloudera.com>
2021-03-17 19:38:50 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace magasprog_2_husvet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
